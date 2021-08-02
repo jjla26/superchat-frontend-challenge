584db77027053b2ac89322ad24811615fc23f9a3
Julio Lopez <julioj.lopeza@gmail.com>
2021-08-02 15:06:41 +0200

    Added an array with the colors the user can select

---
+++ constants/Colors.ts
@@ -0,0 +1,20 @@
+const Colors = [
+  {
+    id: 1,
+    name: 'mustard'
+  },
+  {
+    id: 2,
+    name: 'gray'
+  },
+  {
+    id: 3,
+    name: 'pink'
+  },
+  {
+    id: 4,
+    name: 'blue'
+  }
+]
+
+export default Colors
