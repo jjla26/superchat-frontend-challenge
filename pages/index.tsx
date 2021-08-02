6d55e1c3959fd2afb7418ec65b1801a816c21e94
Julio Lopez <julioj.lopeza@gmail.com>
2021-08-02 18:23:21 +0200

    Added a preview card and styles to the main view

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import LinkForm from '../components/LinkForm';
 import { repoDetails } from '../interfaces/interfaces';
 import { db } from '../utils/firebase'
+import classes from '../styles/Home.module.css'
 
 
 export default function Home() {
@@ -38,10 +39,40 @@ export default function Home() {
   }
   
   return (
-    <div>
-      <h2>Create your link here!</h2>
-      <div>
-        <LinkForm handleSubmit={handleSubmit} handleChangeValues={handleChangeValues} />
+    <div className={classes.homeContainer}>
+      <h1 className={classes.title}>GithubLinks</h1>
+      <div className={classes.content}>
+        <div>
+          <h2>Create your link here!</h2>
+          <div className="card">
+            <LinkForm handleSubmit={handleSubmit} handleChangeValues={handleChangeValues} />
+          </div>
+        </div>
+        <div>{'=>'}</div>
+        <div>
+          <h2>Preview</h2>
+          <div className={`card ${values.color}`}>
+            <div>
+              <p>Repository Owner: {values.username}</p>
+              <p>Repository Name: {values.repo}</p>
+              <p>Stars: 150</p>
+            </div>
+            <div>
+              <h5>Actions</h5>
+              <div className={classes.buttonsContainer}>
+                {values.follow && <button>Follow</button>}
+                {values.star && <button>Star</button>}
+                {values.fork && <button>Fork</button>}
+              </div>
+            </div>
+            <div>
+              <h5>Top 10 Contributors</h5>
+              <ol>
+                {[1,2,3,4,5,6,7,8,9,10].map( contributor => <li key={contributor}>name {contributor}</li>)}
+              </ol>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
