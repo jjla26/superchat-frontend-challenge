18671548b52c053aa9cfffce45be879a471dbbfb
Julio Lopez <julioj.lopeza@gmail.com>
2021-08-02 18:47:58 +0200

    Added mock array contributors to show in the preview

---
+++ constants/Contributors.ts
@@ -0,0 +1,44 @@
+const Contributors = [
+  {
+    name: 'Laura',
+    contributions: 150
+  },
+  {
+    name: 'Julio',
+    contributions: 100
+  },
+  {
+    name: 'Henrik',
+    contributions: 99
+  },
+  {
+    name: 'Sebastian',
+    contributions: 98
+  },
+  {
+    name: 'Maximilian',
+    contributions: 97
+  },
+  {
+    name: 'Ben',
+    contributions: 50
+  },
+  {
+    name: 'Mika',
+    contributions: 45
+  },
+  {
+    name: 'Jonas',
+    contributions: 10
+  },
+  {
+    name: 'Timo',
+    contributions: 8
+  },
+  {
+    name: 'Maurizio',
+    contributions: 5
+  }
+]
+
+export default Contributors
